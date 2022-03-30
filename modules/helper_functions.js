a49ae03941a425c11a70b49c029e16c7607279b6
Joli Holmes <holmesjoli@gmail.com>
2022-03-30 14:40:49 -0400

    added in the collapsible content

--- modules/helper_functions.js
+++ modules/helper_functions.js
@@ -13,3 +13,23 @@ export function read(pth, parse, promises) {
         console.error("unknown file extension");
     }
 }
+
+
+// https://www.w3schools.com/howto/howto_js_collapsible.asp
+export function collapsibleTable() {
+
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+        content.style.display = "none";
+        } else {
+        content.style.display = "block";
+        }
+    });
+    }
+}
