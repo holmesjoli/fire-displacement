6c928769a49b5ed06cb34399c7fd76e5bbab4b58
Joli Holmes <holmesjoli@gmail.com>
2022-04-06 16:26:45 -0400

    referenced scale in the data

--- modules/map.js
+++ modules/map.js
@@ -1,8 +1,10 @@
 // Main map function
-export function build(selector, stateBoundaries) {
+export function build(selector, stateBoundaries, data) {
 
     const width = window.innerWidth*.55;
     const height = window.innerHeight*.85;
+
+    let filteredData = data[0];
 
     // make the SVG and viewbox
     const svg = d3.select(selector)
@@ -19,9 +21,8 @@ export function build(selector, stateBoundaries) {
     // define the settings for map projection
     const projection = d3.geoAlbers()
         .translate([width / 2, height / 2])
-        // .center([0, 5 ])
-        .scale(2000)
-        .center([-15, 44]);
+        .scale(filteredData.scale)
+        .center([filteredData.centerX, filteredData.centerY]);
 
     // create the geo path generator
     let geoPathGenerator = d3.geoPath().projection(projection);
@@ -34,21 +35,33 @@ export function build(selector, stateBoundaries) {
         .enter()
         .append("path")
         .attr("class", 'continent')
-        // draw each country
         .attr("d", geoPathGenerator)
         .attr("country", function (d) { return d.id })
-        // set the color of each country
         .attr("stroke", "#FFFFFF")
-        .attr("stroke-width", .25)
+        .attr("stroke-width", 1)
         .attr("fill", "#D7D7D7");
 
-    var zoom = d3.zoom()
-        .scaleExtent([1, 100])
-        .on('zoom', function (event) {
-            g.attr("transform", "translate(" + event.transform.x + "," + event.transform.y + ")scale(" + event.transform.k + ")");
-        });
+    // var zoom = d3.zoom()
+    //     .scaleExtent([1, 1000])
+    //     .on('zoom', function (event) {
+    //         g.attr("transform", "translate(" + event.transform.x + "," + event.transform.y + ")scale(" + event.transform.k + ")");
+    //     });
 
-    // call zoom so it is "listening" for an event on our SVG
-    svg.call(zoom);
+    // // call zoom so it is "listening" for an event on our SVG
+    // svg.call(zoom);
+}
 
+export function update(data, date) {
+
+    const projection = d3.geoAlbers()
+        .translate([width / 2, height / 2])
+        .scale(7000)
+        .center([-25, 46]);
+
+    // create the geo path generator
+    let geoPathGenerator = d3.geoPath().projection(projection);
+
+    var basemap = g
+        .transition()
+        .attr("d", geoPathGenerator)
 }
