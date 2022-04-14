196695697da8e8dde623158deccac9184b521685
Joli Holmes <holmesjoli@gmail.com>
2022-04-14 16:24:33 -0400

    simplified

--- modules/burn.js
+++ modules/burn.js
@@ -4,20 +4,20 @@ export function createSVG(width, height, data) {
         .attr("viewBox", `0 0 ${width} ${height}`)
         .attr("preserveAspectRatio", "xMidYMid meet");
 
-    const margin = {top: 10, right: 10, bottom: 10, left: 10};
+    const margin = 10;
 
     const extent = d3.extent(data, function(d) {return d.date;})
 
     let scaleR = d3.scaleSqrt()
         .domain([extent[0], extent[1]])
-        .range([margin.left, width - margin.left])
+        .range([margin, width - margin])
 
     svg
         .append("rect")
-        .attr("x", margin.left)
-        .attr("y", margin.top)
-        .attr("width", scaleR(extent[1]))
-        .attr("height", scaleR(extent[1]))
+        .attr("x", margin)
+        .attr("y", margin)
+        .attr("width", scaleR(extent[1]) - margin)
+        .attr("height", scaleR(extent[1])- margin)
         .attr("fill", "#EE2724")
         .attr("stroke", "#473F41")
 
